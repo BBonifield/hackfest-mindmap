8a1ca128d343dd24cb8bb7e3fdfdde0bc897cd4d
Bob Bonifield <bobbonifield@gmail.com>
2013-01-30 20:28:38 -0700

    psh

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HackfestMindmap2::Application.routes.draw do
   match 'new_map' => 'maps#new_map'
-  match 'map/:id' => 'maps#show'
+  match 'map/:id' => 'maps#show', :as => 'map'
 
   root :to => 'maps#index'
 end
